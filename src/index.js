73c44a8ef5e96dfecfc2637f5869dcdf625557c9
Wasif Farooq <wasif.farooq@couriootechnologies.com>
2019-10-22 12:34:13 +0500

    fixing dir structure

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const program = require('commander');
+const encrypt = require('./encrypt');
+const decrypt = require('./decrypt');
+const getFiles = require('./tiverse');
+const paths = require('path');
+
+
+program
+    .option('-m, --mode <mode>', 'encrypt/decrypt the filea or folders', '')
+    .option('-p, --path <path>', 'path of a file or directory', '')
+    .option('-s, --secret <secret>', 'secrect or salt to handle encryption', '')
+    .parse(process.argv);
+
+
+const { mode, path, secret } = program.opts();
+const files = getFiles(paths.resolve(path));
+
+
+if (mode === 'encrypt') {
+    files.map((file) => {
+        encrypt({ file, secret });
+    })
+}
+
+if (mode === 'decrypt') {
+    files.map((file) => {
+        decrypt({ file, secret });
+    });
+}
